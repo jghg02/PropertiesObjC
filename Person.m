352e1c780cbc689b03ccd235efbb324329737f86
Josue Hernandez <josue.hernandez@mercadolibre.cl>
2016-10-10 17:03:00 -0300

    Added a new Methods into implementation File.

--- Person.m
+++ Person.m
@@ -14,4 +14,20 @@
 	isInsecure = YES;
 }
 
+
+//Custom Setter
+-(void)setLastName:(NSString *)lastName{
+	if (![lastName isEqualToString:@""]) {
+		_lastName = lastName;
+	}
+}
+
+//Custom Getter
+-(NSString *)firstName{
+	if ([_firstName isEqualToString:@"HOLA"]) {
+		return @"nene";
+	} else {
+		return @"nena";
+	}
+}
 @end
