4b9398242bd6e382adb11dc4be33beec0c7bc6ce
Josue Hernandez <josue.hernandez@mercadolibre.cl>
2016-11-10 15:37:46 -0300

    Added a new UILabel ito StoryBoard.

--- PropoertiesExample/ViewController.m
+++ PropoertiesExample/ViewController.m
@@ -34,6 +34,15 @@
 	NSLog(@"First Name %@",[p1 firstName]);
 	NSLog(@"Second Name %@",[p1 lastName]);
 	
+	NSString *data = [NSString stringWithFormat:@"%@ %@",[p1 firstName], [p1 lastName]];
+	
+	self.textLabel.text = [@"" stringByAppendingString: @"1\u00a0hour\u00a0ago."];
+	
+	self.textLabel.text = [data stringByAppendingString:@"\u00a0"];
+	
+	//Importante
+	//Link- http://stackoverflow.com/questions/5105053/iphone-uilabel-non-breaking-space
+	//U+00A0 / no-break space / Common Separator, space
 	
 	
 }
